ad5fd632d15089cff63d3a196044694657bbae0a
Krasmir Koeff <kr@koeff.com>
2019-05-17 19:32:06 +0300

    reorganization within App component

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,20 @@
 import React from "react";
 import MainBodyContainerWrapper from "../../components/wrappers/mainBodyContainerWrapper";
 
+import Menu from "../../components/menu/menu";
+import TopBarWrapper from "../../components/wrappers/topBarWrapper";
+import MainWrapper from "../../components/wrappers/mainWrapper";
+
 export default function Dashboard() {
   return (
-    <MainBodyContainerWrapper pageTitle="Dashboard">
-      <p class="m-4">Добре дошли в началната страница</p>
-    </MainBodyContainerWrapper>
+    <MainWrapper>
+      <Menu />
+
+      <TopBarWrapper>
+        <MainBodyContainerWrapper pageTitle="Dashboard">
+          <p class="m-4">Добре дошли в началната страница</p>
+        </MainBodyContainerWrapper>
+      </TopBarWrapper>
+    </MainWrapper>
   );
 }
