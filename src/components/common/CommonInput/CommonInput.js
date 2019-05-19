02cf92baa63c41ad6df6e584b1f6b78c879e310d
Krasmir Koeff <kr@koeff.com>
2019-05-19 18:28:03 +0300

    CommonINput: name, onchange, error

--- src/components/common/CommonInput/CommonInput.js
+++ src/components/common/CommonInput/CommonInput.js
@@ -3,22 +3,35 @@ import PropTypes from "prop-types";
 
 function CommonInput(props) {
   return (
-    <input
-      type={props.type ? props.type : "text"}
-      className={
-        props.className ? props.className : "form-control form-control-user"
-      }
-      id={props.id ? props.id : ""}
-      placeholder={props.placeholder ? props.placeholder : ""}
-    />
+    <div className="form-group">
+      <input
+        type={props.type ? props.type : "text"}
+        name={props.name}
+        onChange={props.onChange}
+        className={
+          props.className ? props.className : "form-control form-control-user"
+        }
+        id={props.id ? props.id : ""}
+        placeholder={props.placeholder ? props.placeholder : ""}
+      />
+      <div
+        className="text-danger"
+        style={{ display: props.error ? "block" : "none" }}
+      >
+        <span>{props.error ? props.error : ""}</span>
+      </div>
+    </div>
   );
 }
 
 CommonInput.propTypes = {
   placeholder: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
   id: PropTypes.string,
   type: PropTypes.string,
-  className: PropTypes.string
+  className: PropTypes.string,
+  error: PropTypes.string
 };
 
 export default CommonInput;
