ef9cd8a825ba63a47345a5117b408abbd1c9658c
Krasmir Koeff <kr@koeff.com>
2019-05-19 18:27:30 +0300

    CommonTextArea props: name, onchange, error

--- src/components/common/CommonTextArea/CommonTextArea.js
+++ src/components/common/CommonTextArea/CommonTextArea.js
@@ -3,20 +3,33 @@ import PropTypes from "prop-types";
 
 function CommonInput(props) {
   return (
-    <textarea
-      className={
-        props.className ? props.className : "form-control form-control-user"
-      }
-      id={props.id ? props.id : ""}
-      placeholder={props.placeholder ? props.placeholder : ""}
-    />
+    <div className="form-group">
+      <textarea
+        name={props.name}
+        onChange={props.onChange}
+        className={
+          props.className ? props.className : "form-control form-control-user"
+        }
+        id={props.id ? props.id : ""}
+        placeholder={props.placeholder ? props.placeholder : ""}
+      />
+      <div
+        className="text-danger"
+        style={{ display: props.error ? "block" : "none" }}
+      >
+        <span>{props.error ? props.error : ""}</span>
+      </div>
+    </div>
   );
 }
 
 CommonInput.propTypes = {
   placeholder: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
   id: PropTypes.string,
-  className: PropTypes.string
+  className: PropTypes.string,
+  error: PropTypes.string
 };
 
 export default CommonInput;
