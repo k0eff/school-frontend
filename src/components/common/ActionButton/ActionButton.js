8a235ffbbfa326740bd4571df0db07af347bed11
Krasmir Koeff <kr@koeff.com>
2019-05-19 18:28:24 +0300

    ActionBUtton optimizations

--- src/components/common/ActionButton/ActionButton.js
+++ src/components/common/ActionButton/ActionButton.js
@@ -1,28 +1,53 @@
 import React from "react";
 import PropTypes from "prop-types";
+import SpinnerInline from "../Spinner/spinner-inline";
 
-function ActionButton(props) {
-  return (
-    <button
-      className={
-        props.className ? props.className : "btn btn-primary btn-icon-split"
-      }
-      id={props.id}
-      onClick={props.onClick}
-    >
-      <span class="icon text-white-50">
-        <i class="fas fa-flag" />
-      </span>
-      <span class="text">{props.text}</span>
-    </button>
-  );
+class ActionButton extends React.Component {
+  spinnerElement = null;
+
+  state = {
+    displaySpinner: false
+  };
+  onClickHandler = () => {
+    this.setState({ displaySpinner: true }); //only show it, do not hide it on the second click
+    // this.props.onClick();
+  };
+
+  render() {
+    return (
+      <div className="form-group">
+        <button
+          className={
+            this.props.className
+              ? this.props.className
+              : "btn btn-primary btn-icon-split"
+          }
+          id={this.props.id}
+          onClick={() => {
+            this.onClickHandler();
+          }}
+        >
+          <span className="icon text-white-50">
+            <i className="fas fa-flag" />
+          </span>
+          <span className="text">{this.props.text}</span>
+        </button>
+
+        {(() => {
+          if (this.state.displaySpinner) {
+            return <SpinnerInline width="32px" className="ml-2" />;
+          }
+        })()}
+      </div>
+    );
+  }
 }
 
 ActionButton.propTypes = {
-  className: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
-  id: PropTypes.string
+  id: PropTypes.string,
+  className: PropTypes.string
 };
 
 export default ActionButton;
