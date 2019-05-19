4e1bdd85a4a75073c5010a94cf9ee9130c7e3963
Krasmir Koeff <kr@koeff.com>
2019-05-19 18:29:40 +0300

    AddKeyToValuePairs - bound

--- src/pages/KeyValue/AddKeyValuePairs.js
+++ src/pages/KeyValue/AddKeyValuePairs.js
@@ -10,11 +10,26 @@ import TopBarWrapper from "../../components/wrappers/topBarWrapper";
 import ActionButton from "../../components/common/ActionButton/ActionButton";
 import CommonInput from "../../components/common/CommonInput/CommonInput";
 import CommonTextArea from "../../components/common/CommonTextArea/CommonTextArea";
+import SpinnerInline from "../../components/common/Spinner/spinner-inline";
 
 export default class AddKeyValuePairs extends Component {
   static propTypes = {
     prop: PropTypes
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      ParamValue: "",
+      ParamDescr: ""
+    };
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
   render() {
     return (
@@ -26,9 +41,17 @@ export default class AddKeyValuePairs extends Component {
             <p className="m-4">Добавяне на предмет</p>
             <div className="p-5">
               <div className="form-group">
-                <CommonInput placeholder="Име" />
+                <CommonInput
+                  placeholder="Име"
+                  name="ParamValue"
+                  onChange={this.handleChange.bind(this)}
+                />
                 <div className="form-group mt-2">
-                  <CommonTextArea placeholder="Кратко описание" />
+                  <CommonTextArea
+                    placeholder="Кратко описание"
+                    name="ParamDescr"
+                    onChange={this.handleChange.bind(this)}
+                  />
                 </div>
                 <div className="form-element">
                   <ActionButton text="Запиши" />
