e7b063b7afc08901659286dd9983f51e67859eb4
Krasmir Koeff <kr@koeff.com>
2019-06-04 18:23:02 +0300

    App - remove unneeded imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import AppStructure from "./components/structure/AppStructure";
 import "./App.css";
 
