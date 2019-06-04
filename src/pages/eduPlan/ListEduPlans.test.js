9974d15cd18f97fb64d89c42c2d3a871b2018b5a
Krasmir Koeff <kr@koeff.com>
2019-06-04 17:23:13 +0300

    ListEduPlans - basic unit tests

--- src/pages/eduPlan/ListEduPlans.test.js
+++ src/pages/eduPlan/ListEduPlans.test.js
@@ -3,13 +3,55 @@ import renderer from "react-test-renderer";
 import AppStructure from "../../components/structure/AppStructure";
 
 import ListEduPlans from "./ListEduPlans";
+import TableData from "../../components/TableData/tableData";
+import Menu from "../../components/menu/menu";
 
 describe("testing component", () => {
   it("should run in test env", () => {
     expect(process.env.NODE_ENV).toEqual("test");
   });
 
-  it("a test for the test", () => {
-    return undefined;
+  let rend, inst;
+
+  beforeEach(() => {
+    rend = renderer.create(
+      <AppStructure>
+        <ListEduPlans />
+      </AppStructure>
+    );
+    inst = rend.root;
+  });
+
+  afterEach(() => {
+    rend.unmount();
+    rend = null;
+  });
+
+  it("shows TableNav ", () => {
+    expect(
+      inst.findByProps({
+        className: "dataTables_info",
+        id: "dataTable_info",
+        role: "status"
+      })
+    ).toBeDefined();
+  });
+
+  it("shows TableData", () => {
+    expect(inst.findByType(TableData)).toBeDefined();
+  });
+
+  it("shows table tag", () => {
+    expect(
+      inst.findByProps({ className: "table table-bordered dataTable" })
+    ).toBeDefined();
+  });
+
+  it("shows loading animation", () => {
+    expect(inst.findByType("img")).toBeDefined();
+  });
+
+  it("has only 1 left menu", () => {
+    expect(inst.findAllByType(Menu).length).toBe(1);
   });
 });
