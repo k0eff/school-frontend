5cbcc0b4bb48d6ed6263a34bbae27cec012f3fd8
Krasmir Koeff <kr@koeff.com>
2019-06-04 18:12:14 +0300

    merge due to rebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
-import store from "./reducers/store";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
+import AppStructure from "./components/structure/AppStructure";
 import "./App.css";
 
 //pages
@@ -25,42 +25,32 @@ import MainWrapper from "./components/wrappers/mainWrapper";
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <MainWrapper>
-          <Menu />
-          <TopBarWrapper>
-            <Switch>
-              <Route exact path="/" component={Dashboard} />
-              <Route path="/blank" component={Blank} />
-              <Route
-                path="/KeyValue/list/:paramName/:pageNum?"
-                component={ListKeyValuePairs}
-              />
-              <Route
-                path="/KeyValue/addEdit/:paramName/:id?"
-                component={AddKeyValuePairs}
-              />
-              <Route path="/eduPlan/list/:pageNum?" component={ListEduPlans} />
-              <Route
-                path="/eduPlan/addEdit/:eduPlanId?"
-                component={AddEduPlans}
-              />
+    <AppStructure>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/blank" component={Blank} />
+        <Route
+          path="/KeyValue/list/:paramName/:pageNum?"
+          component={ListKeyValuePairs}
+        />
+        <Route
+          path="/KeyValue/addEdit/:paramName/:id?"
+          component={AddKeyValuePairs}
+        />
+        <Route path="/eduPlan/list/:pageNum?" component={ListEduPlans} />
+        <Route path="/eduPlan/addEdit/:eduPlanId?" component={AddEduPlans} />
 
-              <Route
-                path="/eduPlanData/list/:eduPlanId/:pageNum?"
-                component={ListEduPlanData}
-              />
-              <Route
-                path="/eduPlanData/addEdit/:eduPlanId/:eduPlanDataId?"
-                component={AddEduPlanData}
-              />
-              <Route component={p404} />
-            </Switch>
-          </TopBarWrapper>
-        </MainWrapper>
-      </BrowserRouter>
-    </Provider>
+        <Route
+          path="/eduPlanData/list/:eduPlanId/:pageNum?"
+          component={ListEduPlanData}
+        />
+        <Route
+          path="/eduPlanData/addEdit/:eduPlanId/:eduPlanDataId?"
+          component={AddEduPlanData}
+        />
+        <Route component={p404} />
+      </Switch>
+    </AppStructure>
   );
 }
 
