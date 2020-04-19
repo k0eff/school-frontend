e8e367016cf9859ea72a0997c4343b0b9df7baf4
Krasimir Koeff <krasimir.koeff.ecollect@gmail.com>
2020-04-19 19:58:06 +0300

    cleanup: remove unused dependencies

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,9 @@
 import React from "react";
 import MainBodyContainerWrapper from "../../components/wrappers/mainBodyContainerWrapper";
 
-import Menu from "../../components/menu/menu";
-import TopBarWrapper from "../../components/wrappers/topBarWrapper";
-import MainWrapper from "../../components/wrappers/mainWrapper";
+// import Menu from "../../components/menu/menu";
+// import TopBarWrapper from "../../components/wrappers/topBarWrapper";
+// import MainWrapper from "../../components/wrappers/mainWrapper";
 
 export default function Dashboard() {
   return (
